a29d84d943b6bfd60ec26b60448f7f9397c7d002
Alejadro Vergara <alejandropucca0@gmail.com>
2024-05-20 19:18:22 -0500

    prettier all

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,7 @@ build = ":TSUpdate",
   config  = function()
     local config = require("nvim-treesitter.configs")
     config.setup ({
-      ensure_installed = { 
-        "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript"
-      },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true }, 
     })
