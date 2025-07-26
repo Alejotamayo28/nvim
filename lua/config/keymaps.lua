6a1eab39a6c944ecf8f14a5c0a66c8282fe1e73f
Alejotamayo28 <alejandropucca0@gmail.com>
2025-07-25 20:18:08 -0500

    git

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,8 @@ keymap.set('n', '<S-p>', '"+p', { noremap = true }) -- Pegar desde el portapapel
 keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
 keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
 
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to window below" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to window above" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
--- lua/plugins/gitsings.lua
+++ lua/plugins/gitsings.lua
@@ -5,24 +5,34 @@ return {
     signs = {
       add    = { text = "┃" },
       change = { text = "┃" },
-      delete = { text = "▁" },
+      delete = { text = "┃" },
     },
     on_attach = function(buffer)
       local gs = package.loaded.gitsigns
 
-      vim.keymap.set("n", "<leader>gp", gs.preview_hunk,
-      { desc = "Preview hunk changes", buffer = buffer })
-
-      vim.keymap.set("n", "<leader>gb", gs.blame_line,
+      -- Autor, fecha y ultimo cambio
+      vim.keymap.set("n", "<leader>gi", gs.blame_line,
         { desc = "Git blame line", buffer = buffer })
-      vim.keymap.set("n", "<leader>gd", gs.diffthis,
-        { desc = "Git diff", buffer = buffer })
 
+      -- Info chunk
+      vim.keymap.set("n", "<leader>go", gs.preview_hunk,
+        { desc = "Preview hunk changes", buffer = buffer })
+
+      -- Undo chunk
+      vim.keymap.set("n", "<leader>gr", gs.reset_hunk,
+        { desc = "Git reset hunk (undo changes)", buffer = buffer })
+
+      -- Diferencias pasado presente
+      vim.keymap.set("n", "<leader>gp", gs.diffthis,
+        { desc = "Git diff", buffer = buffer })
 
       vim.keymap.set("n", "]h", gs.next_hunk,
         { desc = "Next hunk", buffer = buffer })
+
       vim.keymap.set("n", "[h", gs.prev_hunk,
         { desc = "Previous hunk", buffer = buffer })
+
+      vim.keymap.set("n", "<leader>gc", require('telescope.builtin').git_commits, { desc = "Git commits" })
     end,
   },
 }
